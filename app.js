c886d37082eca467ae36d0f38713a9e955005fc3
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-09-15 23:20:55 +0600

    added

--- app.js
+++ app.js
@@ -7,19 +7,40 @@ function setTime(){
 
 
 
-const cards = document.querySelectorAll('.card-main');
+// const cards = document.querySelectorAll('.card-main');
 
-cards.forEach(card => {
-    card.addEventListener('mouseover', () => {
-        const img = card.children[0].children[0].children[0];
-        const imgSrc = img.dataset.img;
-        img.src = `./images/${imgSrc}h.jpg`
+// cards.forEach(card => {
+//     card.addEventListener('mouseover', () => {
+//         const img = card.children[0].children[0].children[0];
+//         const imgSrc = img.dataset.img;
+//         img.src = `./images/${imgSrc}h.jpg`
+//     })
+// })
+// cards.forEach(card => {
+//     card.addEventListener('mouseout', () => {
+//         const img = card.children[0].children[0].children[0];
+//         const imgSrc = img.dataset.img;
+//         img.src = `./images/${imgSrc}.jpg`
+//     })
+// })
+
+
+function colorChange(btn) {
+    const imgColor = btn.dataset.color;
+    
+    const imgClass = btn.dataset.img;
+    const images = document.querySelectorAll("."+imgClass);
+    images.forEach(img => {
+        if (imgColor === 'red') {
+            img.style.filter = "grayscale(100%)"
+        }
+        else if (imgColor === 'green') {
+            img.style.filter = "grayscale(10%)"
+        }
+        else if (imgColor === 'blue') {
+            img.style.filter = "grayscale(50%)"
+        }
+            
     })
-})
-cards.forEach(card => {
-    card.addEventListener('mouseout', () => {
-        const img = card.children[0].children[0].children[0];
-        const imgSrc = img.dataset.img;
-        img.src = `./images/${imgSrc}.jpg`
-    })
-})
+    
+}
